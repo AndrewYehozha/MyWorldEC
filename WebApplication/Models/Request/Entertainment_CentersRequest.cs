fe548c30474ba78186607562ecac3dfaabf7af7a
Andrew <andrew.yehozha@gmail.com>
2019-05-01 22:21:19 +0300

    [task_4] Add CRUD for entertainment center
    - Add entertainment center
    - Edit entertainment center
    - Delete entertainment center
    - Get entertainment center
    - Get entertainment centers list

---
+++ WebApplication/Models/Request/Entertainment_CentersRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication.Models.Response;
+
+namespace WebApplication.Models.Request
+{
+    public class Entertainment_CentersRequest : Entertainment_CentersViewModel
+    {
+    }
+}
